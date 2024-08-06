a332a9ab7f26a2c386f4f306ca7a89e986525bfa
AnnetaDe <anneta.liss@gmail.com>
2024-08-06 23:20:03 +0200

    routerDynamic

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,8 +3,6 @@ import { useSelector } from 'react-redux';
 import { selectIsLoggined } from '../redux/user/userSelectors';
 
 const PrivateRoute = ({ children }) => {
-  const location = useLocation();
-
   const isLoggedIn = useSelector(selectIsLoggined);
   console.log('privat', isLoggedIn);
   return isLoggedIn ? children : <Navigate to="auth/login" state={location} />;
