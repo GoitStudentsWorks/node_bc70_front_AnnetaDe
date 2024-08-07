49df7b686012e489b3dfc45f9a9321b9c2241f57
Yelyzaveta Popkova <156209121+Yelyzaveta31@users.noreply.github.com>
2024-08-07 19:27:51 +0300

    update sidebar + add logo (#29)
    * add sidebar structure
    
    * add sidebar styles css
    
    * add create new board + css
    
    * add structure My Boards
    
    * update sidebar
    
    * update sidebar + add logo
    
    * Update userOperations.js

--- src/redux/user/userOperations.js
+++ src/redux/user/userOperations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { taskProApi } from '../../config/api';
+import axios from 'axios';
 
 export const setToken = accessToken => {
   taskProApi.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
@@ -91,3 +92,62 @@ export const refreshUserThunk = createAsyncThunk(
 // email: 'heidie@modulesdsh.com';
 // name: 'ann';
 // password: 'aaAA1111';
+
+export const fetchBoards = createAsyncThunk(
+  'user/boards',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/api/boards');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addBoard = createAsyncThunk(
+  'boards/addBoard',
+  async ({ title, currentBg, icon }, thunkAPI) => {
+    try {
+      const response = await axios.post('/api/boards', {
+        title,
+        currentBg,
+        icon,
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const editBoard = createAsyncThunk(
+  'boards/editBoard',
+  async ({ title, currentBg, icon, id }, thunkAPI) => {
+    try {
+      const response = await axios.put(`/api/boards/${id}`, {
+        title,
+        currentBg,
+        icon,
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteBoard = createAsyncThunk(
+  'boards/deleteBoard',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/api/boards/${id}`);
+
+      if (response.status === 204) {
+        return id;
+      }
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
