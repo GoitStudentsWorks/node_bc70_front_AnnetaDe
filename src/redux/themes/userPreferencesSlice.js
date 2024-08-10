dc3cc6cf257235f396a1c7e008fa18cce8dd1d20
AnnetaDe <anneta.liss@gmail.com>
2024-08-10 10:49:17 +0200

    Update userPreferencesSlice.js

--- src/redux/themes/userPreferencesSlice.js
+++ src/redux/themes/userPreferencesSlice.js
@@ -32,5 +32,6 @@ const userPreferencesSlice = createSlice({
       });
   },
 });
-export const { changeColorScheme, changeAvatar } = userPreferencesSlice.actions;
+export const { changeColorScheme, changeAvatar, changeUserName } =
+  userPreferencesSlice.actions;
 export const userPreferencesReducer = userPreferencesSlice.reducer;
