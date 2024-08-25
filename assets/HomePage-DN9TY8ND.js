a0b3f81b21df7e70ccb777c4ed898d79ec475616
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-08-25 22:21:09 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_bc70_front_AnnetaDe@917da776cfee2ad8ccadd6688c95e7eb5cec6a6e 🚀

---
+++ assets/HomePage-DN9TY8ND.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-5Ogi1gQu.js";import o from"./ScreensPage-C_nmSV3t.js";const t=()=>(console.log("HomePage"),e.jsx(e.Fragment,{children:e.jsx(o,{})}));export{t as HomePage,t as default};
+//# sourceMappingURL=HomePage-DN9TY8ND.js.map
